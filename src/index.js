681a149def5dad72ad09ab87040b38a33d4ed2b5
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-12 02:30:11 -0700

    add page load listener and form handling functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeService from './js/ExchangeService.js';
+
+
+
+function getFormData(e) {
+  e.preventDefault();
+  const usdInput = document.getElementById('usrUsdIn').value;
+  ExchangeService.getRates();
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", getFormData);
+});
