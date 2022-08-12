031377648a6da98dc85985aea5774d848ee7dea0
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-08-12 04:14:33 -0700

    create error message

--- src/js/ExchangeService.js
+++ src/js/ExchangeService.js
@@ -4,7 +4,7 @@ export default class ExchangeService {
       const resp = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       const jsonResp = await resp.json();
       if (!resp.ok) {
-        let errorMsg = `${resp.status} ${resp.statusText} ${jsonResp.message}`;
+        let errorMsg = `There was an error accessing exchange rate data from API: ${resp.status} ${resp.statusText} ${jsonResp.message}`;
         throw new Error(errorMsg);
       }
       let rates = jsonResp.conversion_rates;
